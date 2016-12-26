7bbdf0016806305ed3e1152887dd805a88fc7b17
你的名字 <luonayin@gmail.com>
2016-12-26 16:43:59 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -48,15 +48,15 @@ class GroupsController < ApplicationController
 
   private
 
-  def find_group_and_check_permieeion
+  def find_group_and_check_permission
     @group = Group.find(params[:id])
 
-    if current_user root_path, alert: "You have no permission."
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
     end
   end
 
-    def group_params
+  def group_params
     params.require(:group).permit(:title, :description)
   end
-
 end
